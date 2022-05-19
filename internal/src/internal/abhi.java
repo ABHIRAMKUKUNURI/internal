02614d266f72b7f274799171912ea7ef5169c54f
vardhan0809 <saivardhan1738@gmail.com>
2022-05-19 11:47:15 +0530

    COMMITED

--- internal/src/internal/abhi.java
+++ internal/src/internal/abhi.java
@@ -2,7 +2,7 @@ package internal;
 
 public class abhi {
 	public static void main(String args[]) {
-		System.out.println("hello JKworld25!");
+		System.out.println("hello THIS JKworld25!");
 	}
 
 }
