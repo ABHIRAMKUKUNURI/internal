d011124dd6147d7bc34d9af09f09336b32632025
vardhan0809 <saivardhan1738@gmail.com>
2022-05-19 11:49:58 +0530

    LAST ONE

--- internal/src/internal/abhi.java
+++ internal/src/internal/abhi.java
@@ -2,7 +2,7 @@ package internal;
 
 public class abhi {
 	public static void main(String args[]) {
-		System.out.println("hello THIS JKworld25!");
+		System.out.println("hello THIS JK ABHI world25!");
 	}
 
 }
